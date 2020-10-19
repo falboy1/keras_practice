e2bb6f7d6baad0bfcf9cbd7016cdb1ac61c1a011
haya-gpu-server <10haya22ton@gmail.com>
2020-10-19 14:49:51 +0900

    転移学習するかしないかを指定できるようにget_model()を変更

--- models/resnet_transfer.py
+++ models/resnet_transfer.py
@@ -4,7 +4,7 @@ from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Input, GlobalAveragePooling2D, Dense
 
 
-def get_model(img_width, img_height, num_classes):
+def get_model(img_width, img_height, num_classes, transfer=True):
     # ネットワーク作成
     input_tensor = Input(shape=(img_width, img_height, 3))
     base_model = ResNet50(include_top=False, weights='imagenet',input_tensor=input_tensor)
@@ -16,8 +16,11 @@ def get_model(img_width, img_height, num_classes):
     # モデルの定義
     model = Model(inputs=base_model.input, outputs=predictions)
 
-    # ベースモデルの重みを固定化
-    for layer in base_model.layers:
-        layer.trainable = False
+    if transfer is True:
+        # ベースモデルの重みを固定化
+        for layer in base_model.layers:
+            layer.trainable = False
+    else:
+        pass
 
     return model
