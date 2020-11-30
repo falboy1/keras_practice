677acd7d565357a19c5b653e646e78a04165b8fe
haya-gpu-server <10haya22ton@gmail.com>
2020-11-30 16:18:15 +0900

    タイプミス修正

--- scripts/visualization.py
+++ scripts/visualization.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
-import matplotlib.font_manager as fm
-import matplotlib.patheffects as path_effects
-from matplotlib import font_manager
-from sklearn.metrics import classification_report, confusion_matrix
+import itertools
+from sklearn.metrics import  confusion_matrix
 import numpy as np
 
 
-# 学習曲線を画像保存する関数
+# historyオブジェクトから学習曲線を画像保存する関数
 def plot_history(filename, histories ,key='loss'):
     plt.figure(figsize=(16,10))
     for name, history in histories:
@@ -16,19 +14,19 @@ def plot_history(filename, histories ,key='loss'):
         plt.plot(history.epoch, history.history[key], color=val[0].get_color(),
                     label=name.title()+' Train')
 
-    plt.xlabel('Epochs')
-    plt.ylabel(key.replace('_',' ').title())
+    plt.xlabel('Epochs', fontsize='large')
+    plt.ylabel(key.replace('_',' ').title(), fontsize='large')
     plt.legend()
 
     plt.xlim([0,max(history.epoch)])
     plt.savefig(filename)
     plt.show()
 
-'''
+
 # 混合行列を画像保存する関数
-def plot_confusion_matrix(cm, classes, normalize=False, title='confusion_matrix', cmap=plt.cm.Oranges):
+def plot_confusion_matrix(cm, classes, filename, normalize=False, title='confusion_matrix', cmap=plt.cm.Greens):
     plt.figure(figsize=(14,10))
-    plt.imshow(cm, interpolation='nearest', cmap=map)
+    plt.imshow(cm, cmap=cmap)
     plt.title(title, fontsize=25)
     cb = plt.colorbar()
     cb.ax.tick_params(labelsize=15)
@@ -38,15 +36,17 @@ def plot_confusion_matrix(cm, classes, normalize=False, title='confusion_matrix'
 
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-    
-    thresh = cm.max() / 2.
 
-    for i, j itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, cm[i, j], horizontalalignment='center',
-         fontsize=15, color='white' if cm[i, j] > thresh else 'black'
-        )
+    thresh = cm.max() /2.
+
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                horizontalalignment="center", fontsize=15,
+                color="white" if cm[i, j] > thresh else "black")
+
 
     plt.tight_layout()
-    plt.xlabel('true', fontsize=25)
-    plt.ylabel('pred', fontsize=25)
-'''
+    plt.xlabel("ground_truth", fontsize=25)
+    plt.ylabel("predict", fontsize=25)
+    plt.savefig(filename)
+    
